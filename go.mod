e8c55bb6947bebbdee4b4e35c61290d1bf10a7a1
Liam Mackie <liam.mackie@octopus.com>
2026-04-14 11:02:00 +1000

    feat: add config loading and validation for YAML definitions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/liam-mackie/helmrunner
 
 go 1.24.4
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,96 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Variable struct {
+	Name        string `yaml:"name"`
+	Description string `yaml:"description"`
+	Default     string `yaml:"default"`
+}
+
+type Chart struct {
+	Source     string `yaml:"source"`
+	Repository string `yaml:"repository"`
+	Name       string `yaml:"name"`
+	Version    string `yaml:"version"`
+}
+
+type Definition struct {
+	Name      string                 `yaml:"name"`
+	Release   string                 `yaml:"release"`
+	Namespace string                 `yaml:"namespace"`
+	Chart     Chart                  `yaml:"chart"`
+	Variables []Variable             `yaml:"variables"`
+	Values    map[string]interface{} `yaml:"values"`
+}
+
+func Load(dir string) ([]Definition, error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("reading directory %s: %w", dir, err)
+	}
+
+	var defs []Definition
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		ext := strings.ToLower(filepath.Ext(entry.Name()))
+		if ext != ".yaml" && ext != ".yml" {
+			continue
+		}
+
+		data, err := os.ReadFile(filepath.Join(dir, entry.Name()))
+		if err != nil {
+			return nil, fmt.Errorf("reading %s: %w", entry.Name(), err)
+		}
+
+		var def Definition
+		if err := yaml.Unmarshal(data, &def); err != nil {
+			return nil, fmt.Errorf("parsing %s: %w", entry.Name(), err)
+		}
+
+		if def.Release == "" {
+			def.Release = def.Name
+		}
+		if def.Namespace == "" {
+			def.Namespace = "default"
+		}
+
+		if err := validate(def, entry.Name()); err != nil {
+			return nil, err
+		}
+
+		defs = append(defs, def)
+	}
+
+	return defs, nil
+}
+
+func validate(def Definition, filename string) error {
+	if def.Name == "" {
+		return fmt.Errorf("%s: name is required", filename)
+	}
+
+	hasSource := def.Chart.Source != ""
+	hasRepo := def.Chart.Repository != ""
+
+	if !hasSource && !hasRepo {
+		return fmt.Errorf("%s: chart.source or chart.repository is required", filename)
+	}
+	if hasSource && hasRepo {
+		return fmt.Errorf("%s: chart.source and chart.repository are mutually exclusive", filename)
+	}
+	if hasRepo && def.Chart.Name == "" {
+		return fmt.Errorf("%s: chart.name is required when using chart.repository", filename)
+	}
+
+	return nil
+}
